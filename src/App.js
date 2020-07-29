91bca887a15b9470a907edb613a95088410d94f2
dcbentley <danielcbentley@gmail.com>
2020-07-28 23:16:46 -0400

    added proposal MD and updated ReadMe other minor cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <header>
-        <nav>
-          Nav goes here
-        </nav>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super (); 
+      this.state = {
+        placeHolder: null,
+    }
+}
+componentDidMount() {
+  const URL =
+  'https://cors-anywhere.herokuapp.com/https://haveibeenpwned.com/api/v3/breachedaccount/danielcbentley@gmail.com';
+  const headers = {
+		'Content-Type': 'application/json',
+		'hibp-api-key': process.env.REACT_APP_UNNAMED_PWNED_APP_API_KEY,
+	};
+  fetch(URL, {
+    method: 'GET',
+    headers: headers,
+  })
+  .then(res => res.json())
+  .then(console.log)
+}
+
+
+  render() {
+    console.log();
+
+    return (
+      <div>
+        <header>
+          <nav>
+            Nav goes here
+          </nav>
+        </header>
+  
+  
+      </div>
+    );
+  }
 }
 
 export default App;
